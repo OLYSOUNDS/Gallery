5debedd4c0bdb07b6e6521da99b589f6449fd67f
OLYSOUNDS <liannasylvan@gmail.com>
2023-09-04 14:10:13 -0500

    add files to gitattribut folder

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1848138.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 148532995, 148533003, 148533005, 148533019, 148533022, 148533029, 148533031, 148533032 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/148532992/1/basis.wasm.js', 'wasmUrl' : 'files/assets/148532993/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/148532991/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/148532988/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/148532989/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/148532987/1/ammo.js', 'preload' : true},
+];
